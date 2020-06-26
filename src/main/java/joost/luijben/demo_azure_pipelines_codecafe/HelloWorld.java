27b9f944896f0f3bf727aa681a3811ccf86e2dc2
Joost <joost.luijben@student.hu.nl>
2020-06-26 14:33:18 +0200

    Add helloworld controller

--- src/main/java/joost/luijben/demo_azure_pipelines_codecafe/HelloWorld.java
+++ src/main/java/joost/luijben/demo_azure_pipelines_codecafe/HelloWorld.java
@@ -1,4 +1,12 @@
 package joost.luijben.demo_azure_pipelines_codecafe;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloWorld {
+    @GetMapping
+    public String helloWorld() {
+        return "Hello World";
+    }
 }
