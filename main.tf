081c9e16ff977275f375b14222a51cbd9ddb8225
Joost <joost.luijben@student.hu.nl>
2020-09-18 15:07:08 +0200

    Test

--- main.tf
+++ main.tf
@@ -3,19 +3,26 @@ provider "aws" {
   version = "~> 3.0"
 }
 
-resource "aws_ecr_repository" "test_terraform" {
-  name                 = "test_terraform"
-  image_tag_mutability = "MUTABLE"
+//resource "aws_ecr_repository" "test_terraform" {
+//  name                 = "test_terraform"
+//  image_tag_mutability = "MUTABLE"
+//
+//  image_scanning_configuration {
+//    scan_on_push = true
+//  }
+//}
+//
+//resource "aws_ecs_cluster" "cluster_terraform" {
+//  name = "cluster_terraform"
+//}
 
-  image_scanning_configuration {
-    scan_on_push = true
-  }
+data "aws_ecr_repository" "test_terraform" {
+  name = "test_terraform"
+
 }
 
-resource "aws_ecs_cluster" "cluster_terraform" {
-  name = "cluster_terraform"
-}
-
-output "test_terraform_output_url" {
-  value = aws_ecr_repository.test_terraform.repository_url
-}
+//output "test_terraform_output_url" {
+//  value = aws_ecr_repository.test_terraform.repository_url
+//}
+//
+//
